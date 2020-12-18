59ab23465b7a8c08ec7eb917e431f2d79252f89b
Brandon Barker <beb82@cornell.edu>
2020-12-18 16:56:30 +0000

    initial checkin of Rust cp

---
+++ Rust/sfw-tools/src/cp.rs
@@ -0,0 +1,34 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+
+use read_byte_slice::{ByteSliceIter, FallibleStreamingIterator};
+
+fn main() -> std::io::Result<()> {
+    let mut args = env::args();
+    args.next();
+    let src = args.next().expect("cp: missing source");
+    let dst = args.next().expect("cp: missing destination");
+    cp(src, dst);
+    Ok(())
+}
+
+fn cp(src: String, dst: String) -> () {
+    let f_in =
+        File::open(&src).expect(&format!("Couldn't open source: {}", &src));
+
+    let mut f_in_iter = ByteSliceIter::new(f_in, 4096);
+    let mut f_out = File::create(&dst)
+        .expect(&format!("Couldn't open destination: {}", &dst));
+
+    loop {
+        match f_in_iter.next() {
+            Ok(Some(b_slice)) => {f_out
+                .write(b_slice)
+                .expect(&format!("Failure writing to {}.", &dst));}
+            Ok(None) => {break;}
+            Err(err) => panic!("Failure reading from {}: {}.", &src, err),
+        }
+    }
+    return ();
+}

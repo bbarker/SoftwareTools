75f7173693db46f8e86f25614dc04604b4bb3482
Brandon Barker <beb82@cornell.edu>
2020-12-20 16:16:09 +0000

    functional approach for cp using from_iter

--- Rust/sfw-tools/src/cp.rs
+++ Rust/sfw-tools/src/cp.rs
@@ -1,5 +1,8 @@
+#![deny(unused_must_use)]
+
 use std::env;
 use std::fs::File;
+use std::io::Result;
 use std::io::Write;
 
 use read_byte_slice::{ByteSliceIter, FallibleStreamingIterator};
@@ -9,25 +12,23 @@ fn main() -> () {
     args.next();
     let src = args.next().expect("cp: missing source");
     let dst = args.next().expect("cp: missing destination");
-    cp(src, dst);
+    match cp(src, dst) {
+        Ok(_) => (),
+        Err(err) => panic!("Failure in cp: {}", err),
+    }
 }
 
-fn cp(src: String, dst: String) -> () {
+fn cp(src: String, dst: String) -> Result<()> {
     let f_in =
         File::open(&src).expect(&format!("Couldn't open source: {}", &src));
 
-    let mut f_in_iter = ByteSliceIter::new(f_in, 4096);
+    let f_in_iter = ByteSliceIter::new(f_in, 4096);
     let mut f_out = File::create(&dst)
         .expect(&format!("Couldn't open destination: {}", &dst));
 
-    loop {
-        match f_in_iter.next() {
-            Ok(Some(b_slice)) => {f_out
-                .write(b_slice)
-                .expect(&format!("Failure writing to {}.", &dst));}
-            Ok(None) => {break;}
-            Err(err) => panic!("Failure reading from {}: {}.", &src, err),
-        }
-    }
-    return ();
+    f_in_iter.for_each(|b_slice| {
+        f_out
+            .write(b_slice)
+            .expect(&format!("Failure writing to {}.", &dst));
+    })
 }

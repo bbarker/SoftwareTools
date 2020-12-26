79c51537538f7ccb22ffcb2137fba578f608d2c1
Brandon Barker <beb82@cornell.edu>
2020-12-26 03:16:51 +0000

    adding a convenience function for formatting user errors

--- Rust/sfw-tools/src/main.rs
+++ Rust/sfw-tools/src/main.rs
@@ -1,15 +1,9 @@
 #![deny(unused_must_use)]
 
-use sfwtools::cp;
-use std::env;
-use std::io::{Error, ErrorKind::InvalidInput};
-use std::process;
+use sfwtools::{cp, get_args, SfwRes};
 
 fn main() -> () {
-    let (cmd, args) = get_args().unwrap_or_else(|err| {
-        println!("Argument error: {}", err);
-        process::exit(1)
-    });
+    let (cmd, args) = get_args().user_err("Argument error");
     print!("cmd = {}, args = {:?}", cmd, args);
 
     // let src = args.next().expect("cp: missing source");
@@ -18,13 +12,4 @@ fn main() -> () {
     //     Ok(_) => (),
     //     Err(err) => panic!("Failure in cp: {}", err),
     // }
-}
-
-fn get_args() -> Result<(String, Vec<String>), Error> {
-    let mut args_in = env::args();
-    let cmd = args_in
-        .next()
-        .ok_or_else(|| Error::new(InvalidInput, "Impossible: no first arg!"))?;
-    let args_out: Vec<String> = args_in.collect::<Vec<String>>();
-    Ok((cmd, args_out))
 }
--- Rust/sfw-tools/src/sfwtools.rs
+++ Rust/sfw-tools/src/sfwtools.rs
@@ -1,12 +1,24 @@
 #![deny(unused_must_use)]
 
+use std::env;
+use std::fmt::Display;
 use std::fs::File;
-use std::io::Result;
 use std::io::Write;
+use std::io::{Error, ErrorKind::InvalidInput};
+use std::process;
 
 use read_byte_slice::{ByteSliceIter, FallibleStreamingIterator};
 
-pub fn cp(src: String, dst: String) -> Result<()> {
+pub fn get_args() -> Result<(String, Vec<String>), Error> {
+    let mut args_in = env::args();
+    let cmd = args_in
+        .next()
+        .ok_or_else(|| Error::new(InvalidInput, "Impossible: no first arg!"))?;
+    let args_out: Vec<String> = args_in.collect::<Vec<String>>();
+    Ok((cmd, args_out))
+}
+
+pub fn cp(src: String, dst: String) -> Result<(), Error> {
     let f_in =
         File::open(&src).expect(&format!("Couldn't open source: {}", &src));
 
@@ -19,4 +31,28 @@ pub fn cp(src: String, dst: String) -> Result<()> {
             .write(b_slice)
             .expect(&format!("Failure writing to {}.", &dst));
     })
+}
+
+const USER_ERROR_CODE: i32 = 1;
+
+pub trait SfwRes<T, E: Display> {
+    fn unwrap_or_else<F: FnOnce(E) -> T>(self, op: F) -> T;
+
+    // you can write a trait of your own and implement that trait for
+    // Result<T, E> where E: Display
+    fn user_err(self, fstr: &str) -> T
+    where
+        Self: Sized,
+    {
+        self.unwrap_or_else(|err| {
+            eprintln!("{}: {}", fstr, err);
+            process::exit(USER_ERROR_CODE)
+        })
+    }
+}
+
+impl<T, E: Display> SfwRes<T, E> for Result<T, E> {
+    fn unwrap_or_else<F: FnOnce(E) -> T>(self, op: F) -> T {
+        self.unwrap_or_else(op)
+    }
 }

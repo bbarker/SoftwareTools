7a688377b18b73e2b0a556a880f2e12bd5b2e179
Brandon Barker <beb82@cornell.edu>
2021-03-08 17:47:43 +0000

    adding exectuble code for calling detab

--- Rust/sfw-tools/src/tabs.rs
+++ Rust/sfw-tools/src/tabs.rs
@@ -2,8 +2,7 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{Error, Read, Write};
 
-// use seahorse::{App, Command, Context};
-// use byteorder::WriteBytesExt;
+use seahorse::{App, Command, Context};
 use tailcall::tailcall;
 
 use crate::bytes_iter::BytesIter;
@@ -11,11 +10,44 @@ use crate::constants::*;
 use crate::error::*;
 use crate::util::write_u8;
 
-pub fn detab(src: &str, dst: &str) -> Result<(), Error> {
+pub fn detab_app() -> App {
+    App::new("detab")
+        .author("Brandon Elam Barker")
+        .action(run_detab_seahorse_action)
+        .command(run_detab_seahorse_cmd())
+}
+
+const DETAB_USAGE: &str = "detab [SOURCE_FILE] [DEST_FILE]";
+
+pub fn run_detab_seahorse_cmd() -> Command {
+    Command::new("detab")
+        .description("detab: remove tabs from a file; output to STDOUT")
+        .usage(DETAB_USAGE)
+        .action(run_detab_seahorse_action)
+}
+
+pub fn run_detab_seahorse_action(ctxt: &Context) {
+    let mut args = ctxt.args.iter();
+    let src = args.next().user_err("detab: missing source");
+    let f_out: Box<dyn Write> = match args.next() {
+        Some(dst) => Box::new(
+            File::create(&dst)
+                .user_err(&*format!("Couldn't open destination: {}", &dst)),
+        ),
+        None => Box::new(std::io::stdout()),
+    };
+    run_detab(&src, f_out);
+}
+
+/// Convenience function for running detab in idiomatic fashion
+/// (i.e.) errors are printed to user and the program exits.
+pub fn run_detab(src: &str, dst: Box<dyn Write>) {
+    detab(src, dst).user_err("Error in detab");
+}
+
+pub fn detab<W: Write>(src: &str, mut f_out: W) -> Result<(), Error> {
     let f_in = File::open(&src).sfw_err("Couldn't open source")?;
     let mut f_in_iter = BytesIter::new(f_in, DEFAULT_BUF_SIZE);
-    let mut f_out = File::create(&dst)
-        .sfw_err(&*format!("Couldn't open destination: {}", &dst))?;
 
     f_in_iter.try_for_each(|b_slice_res| match b_slice_res {
         Ok(b_slice) => f_out.write_all(&b_slice),
